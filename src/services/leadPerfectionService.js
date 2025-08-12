1d234af4d2535269fd12d5ad102c34d2bd5d5c72
Shoki <shoki@Shokis-MacBook-Air.local>
2025-08-12 22:49:19 +0800

    test

--- src/services/leadPerfectionService.js
+++ src/services/leadPerfectionService.js
@@ -263,6 +263,7 @@ export const formatAppointmentForLeadPerfection = (formData) => {
     apptdate,
     appttime,
     sender: 362
+    
   };
 
   // Add notes field only if sweepstakesOnly is true
